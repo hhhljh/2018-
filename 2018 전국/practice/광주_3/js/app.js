72c909069b45483b491f29035c4806afa617bdaf
hhhljh <01jemma@naver.com>
2018-10-10 13:32:35 +0900

    1010

--- 2018 전국/practice/광주_3/js/app.js
+++ 2018 전국/practice/광주_3/js/app.js
@@ -10,6 +10,9 @@ function loadFunction (){
 	imgMove();
 	scrollAnimate();
 	iconMove();
+	imgOver();
+	splitTxt();
+	typingTxt();
 }
 
 function backHover(){
@@ -36,6 +39,7 @@ function scrollAnimate(){
 		var thisHref = $(this).attr('href')
 		var scrollOn = $(thisHref).offset().top;
 		$('html, body').animate({scrollTop : scrollOn},600);
+		var thisTarget = function(){location.href = thisHref}
 	})
 
 }
@@ -49,6 +53,40 @@ function iconMove(){
 		$(this).css({'margin-top':'00px', 'transition':''});
 	}) 
 }
+
+function imgOver(){
+	var back = $(".col-md-3.col-sm-6.col-xs-12.wow.fadeIn .portfolio-thumb");
+	var colorBack = $(".col-md-3.col-sm-6.col-xs-12.wow.fadeIn .portfolio-thumb .portfolio-overlay");
+	$(colorBack).mouseenter(function(){
+		$(this).css("opacity","1");
+	})
+	$(colorBack).mouseleave(function(){
+		$(this).css("opacity","0");
+	})
+}
+
+function splitTxt(){
+	var allText = $(".element .sub-element").text();
+	var text = allText.split("다.");
+	var textReal0 = text[0]+"다.";
+	var textReal1 = text[1]+"다.";
+	var textReal2 = text[2]+"다.";
+	console.log(textReal2);
+	function typingTxt(cont1,cont2,speed){
+		var Otxt = cont1.text();
+		var Ocont = Otxt.split("");
+		var i = 0;
+		function show(){
+			if(i<Ocont.length){
+				cont2.append(Ocont[i]);
+				i++;
+			};
+		};
+		var Otimer = setInterval(show,speed);
+	};
+	typingTxt();
+}
+
 
 $(loadFunction)
 .on('mouseenter mouseleave', '#service div.col-md-4', function () {
