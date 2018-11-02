76aaf59b364ea2a5eb1e59804e18866bab065342
hhhljh <01jemma@naver.com>
2018-11-02 17:54:41 +0900

    1102

--- 2018 전국/practice/광주_3/js/app.js
+++ 2018 전국/practice/광주_3/js/app.js
@@ -1,11 +1,44 @@
 function loadFunction (){
-	$("head").append('\
-		<style>\
-		#top>.navbar{position:sticky;top:0;}\
-		ul.navbar-right li a:hover{color:#ff4e00 !important;}\
-		#contact #message{resize: vertical;}\
-		</style>\
-		')
+	/*로딩애니메이션:각 div의 크기 조절 애니메이션*/
+	// $('body').prepend(`
+	// 	<div id="loading">
+	// 		<div></div><div></div><div></div><div></div><div></div>
+	// 	</div>
+	// `)
+	$("head").append(`
+		<style>
+			#top>.navbar{position:sticky;top:0;}
+			ul.navbar-right li a:hover{color:#ff4e00 !important;}
+			#contact #message{resize: vertical;}
+			#service div.col-md-4:nth-child(3){background:#505050;}
+			/*로딩애니메이션*/
+			#loading {animation:loading 0.6s both;animation-delay:0.3s;background:#fff;position:fixed;left:0;right:0;top:0;bottom:0;display:flex;justify-content:center;align-items:center;}
+			#loading div {width:5px;height:20px;background:#000;margin:0 1px;}
+			#loading div:nth-child(1){animation:loadingChild 0.35s both linear;}
+			#loading div:nth-child(2){animation:loadingChild 0.35s both linear;animation-delay:0.05s}
+			#loading div:nth-child(3){animation:loadingChild 0.35s both linear;animation-delay:0.1s}
+			#loading div:nth-child(4){animation:loadingChild 0.35s both linear;animation-delay:0.15s}
+			#loading div:nth-child(5){animation:loadingChild 0.35s both linear;animation-delay:0.2s}
+
+			@keyframes loading {
+				0% {opacity:1;z-index:100;}
+				99% {opacity:0;z-index:100;}
+				100% {opacity:0;z-index:-100;display:none;}
+			}
+
+			@keyframes loadingChild {
+				14.29% {height:20px;}
+				28.58% {height:30px;}
+				42.87% {height:40px;}
+				57.16% {height:50px;}
+				71.45% {height:40px;}
+				85.74% {height:30px;}
+				100% {height:20px;}
+			}
+		</style>
+	`)
+
+	memberChk()
 	backHover();
 	imgMove();
 	scrollAnimate();
@@ -72,22 +105,121 @@ function imgOver(){
 
 /*타이핑 효과_문장 나누기*/
 function splitTxt(){
-	var allText = $(".element .sub-element").text();
-	var text = allText.split("다.");
-	var real0 = text[0]+"다.";
-	var real1 = text[1]+"다.";
-	var real2 = text[2]+"다.";
-	console.log(textReal2);
+	var txts = $('.sub-element')
+	var saved = []
+	txts.each(function () {
+		saved.push(this.innerHTML)
+	})
+	$('.element').empty()
+	var delay = 0
+	var text = ""
+	function textSet (ele, i) {
+		var text = ele.substring(0, i)
+		setTimeout(function () {
+			$('.element').html(text)
+		}, delay += 100)
+	}
+	/*문장을 타이핑 효과로 차례로 가져오기*/
+	function play () {
+		delay = 0
+		saved.forEach(function (ele) {
+			for (var i = 0; i < ele.length; i++) {
+				textSet(ele, i)
+			}
+			for (var i = ele.length - 1; i >= 0; i--) {
+				textSet(ele, i)
+			}
+		})
+		setTimeout(function () {
+			play()
+		}, delay)
+	}
+	play()
 }
 
+/*textarea 길이 조절*/
 function messageBox(){
 	var rows = $("textarea").val().split("\n").length
 	$("textarea").attr("rows",rows)
 }
 
+/*로그인 기능*/
+function login () {
+	var id = $('#userid').val()
+	var pw = $('#pw').val()
+	if( id == 'admin' && pw == '1234'){
+		alert('로그인 되었습니다.')
+		sessionStorage.setItem('member', 'true')
+		location.reload()
+	} else {
+		alert('아이디 또는 비밀번호가 일치하지 않습니다.')
+		location.reload()
+	}
+}
+
+/*로그아웃 기능*/
+function logout () {
+	sessionStorage.removeItem('member')
+	alert('로그아웃 되었습니다.')
+	location.reload()
+}
+
+/*멤버 확인*/
+function memberChk () {
+	var isMember = sessionStorage.getItem('member')
+	if (isMember === 'true') {
+		$('.mb-btn .login-btn, .mb-btn .join-btn').hide()
+	} else {
+		$('.mb-btn .logout-btn').hide()
+	}
+}
+
+/*function galleryUse(){
+
+}*/
 
 $(loadFunction)
+.on('click','a[href="#"]',  function (e) {
+	e.preventDefault()
+})
 .on('mouseenter mouseleave', '#service div.col-md-4', function () {
 	$(this).toggleClass('active')
 })
-.on('dblclick','textarea',messageBox)
+.on('dblclick keyup','textarea',messageBox)
+.on('click', '.modal-content  .login-btn', login)
+.on('click', '.mb-btn  .logout-btn', logout)
+.on('click','#portfolio .portfolio-thumb .portfolio-overlay', function () {
+	$('.gallery-area').css('display','block');
+})
+.on('dragover mouseover','.gallery-area',  function (e) {
+	e.preventDefault()
+})
+.on('drop','.gallery-area',  function (e) {
+	e.preventDefault()
+
+	let files = e.originalEvent.dataTransfer.files
+	console.log(files)
+
+	for (let i = 0, f; f = files[i]; i++) {
+		const reader = new FileReader()
+
+		reader.onload = (theFile => {
+			console.log(theFile.name)
+			console.log(theFile.size)
+			return evt => {
+				$('.gallery-area .now-img').prepend(`
+                    <img src="${evt.target.result}" alt="pt1">
+				`)
+				$('.gallery-area .img-list ul').prepend(`
+                    <li>
+                        <img src="${evt.target.result}" alt="pt1">
+                        <span class="gallery-remove">x</span>
+                    </li>
+				`)
+			}
+		})(f)
+
+		reader.readAsDataURL(f)
+	}
+
+})
